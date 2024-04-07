82497147d3f7492d4068752b3dfd298c36f4b1fc
Jono Luiz Cosio <jonoluiz@gmail.com>
2024-04-07 09:42:01 +0800

    Add tailwind and skeletonui

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config}*/
+import { join } from 'path';
+import type { Config } from 'tailwindcss';
+import { skeleton } from '@skeletonlabs/tw-plugin';
+
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve(
+		'@skeletonlabs/skeleton'),
+		'../**/*.{html,js,svelte,ts}'
+	)],
+
+	theme: {
+		extend: {}
+	},
+
+	plugins: [skeleton]
+} satisfies Config;
+
+module.exports = config;
