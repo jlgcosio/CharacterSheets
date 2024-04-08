d3ca32e494acef56228b06ac2c542e4d55c1957d
Jono Luiz Cosio <jonoluiz@gmail.com>
2024-04-08 23:20:04 +0800

    Add passive effect

--- src/lib/components/monsterguts/Weapon.svelte
+++ src/lib/components/monsterguts/Weapon.svelte
@@ -16,6 +16,10 @@
 		dirty = false;
 	}
 
+	function toggleDirty() {
+		dirty = !dirty;
+	}
+
 	function resetHp() {
 		weapon.health.current = weapon.health.max;
 		dirty = true;
@@ -33,14 +37,15 @@
 			<input
 				type="text"
 				class="input input-bordered flex-1"
+				placeholder="Weapon name"
 				bind:value={weapon.weaponName}
-				on:change={() => (dirty = true)}
+				on:change={toggleDirty}
 			/>
 			<button class="btn btn-warning" on:click={removeWeapon}>Delete</button>
 		</div>
 	</div>
 	<div class="flex w-full gap-4">
-		<div class="flex-1">
+		<div class="flex flex-1 flex-col gap-2">
 			<label class="form-control">
 				<span class="inline">Health</span>
 				<div class="flex flex-row items-center gap-4">
@@ -49,7 +54,7 @@
 						class="input input-bordered"
 						bind:value={weapon.health.current}
 						min={0}
-						on:change={() => (dirty = true)}
+						on:change={toggleDirty}
 					/>
 					<span>/</span>
 					<input
@@ -57,13 +62,11 @@
 						class="input input-bordered"
 						bind:value={weapon.health.max}
 						min={0}
-						on:change={() => (dirty = true)}
+						on:change={toggleDirty}
 					/>
 					<button class="btn" on:click={resetHp}>Reset HP</button>
 				</div>
 			</label>
-		</div>
-		<div class="flex-1">
 			<label class="form-control">
 				<span class="inline">{weapon.resourceType}</span>
 				<div class="flex flex-row items-center gap-4">
@@ -72,7 +75,7 @@
 						class="input input-bordered"
 						bind:value={weapon.resource.current}
 						min={0}
-						on:change={() => (dirty = true)}
+						on:change={toggleDirty}
 					/>
 					<span>/</span>
 					<input
@@ -80,11 +83,32 @@
 						class="input input-bordered"
 						bind:value={weapon.resource.max}
 						min={0}
-						on:change={() => (dirty = true)}
+						on:change={toggleDirty}
 					/>
 					<button class="variant-outline btn" on:click={resetResource}
 						>Reset {weapon.resourceType}</button
 					>
+				</div>
+			</label>
+		</div>
+		<div class="flex-1">
+			<label class="form-control">
+				<p>Passive</p>
+				<div class="grid grid-cols-3 gap-2">
+					<input
+						type="text"
+						class="input input-bordered col-span-1"
+						placeholder="Passive Effect"
+						bind:value={weapon.passive.name}
+						on:change={toggleDirty}
+					/>
+					<input
+						type="text"
+						class="input input-bordered col-span-2"
+						placeholder="Effect Description"
+						bind:value={weapon.passive.description}
+						on:change={toggleDirty}
+					/>
 				</div>
 			</label>
 		</div>
@@ -98,7 +122,7 @@
 					class="input input-bordered"
 					bind:value={weapon.crush}
 					min={0}
-					on:change={() => (dirty = true)}
+					on:change={toggleDirty}
 				/>
 			</label>
 		</div>
@@ -110,7 +134,7 @@
 					class="input input-bordered"
 					bind:value={weapon.slice}
 					min={0}
-					on:change={() => (dirty = true)}
+					on:change={toggleDirty}
 				/>
 			</label>
 		</div>
@@ -122,7 +146,7 @@
 					class="input input-bordered"
 					bind:value={weapon.pierce}
 					min={0}
-					on:change={() => (dirty = true)}
+					on:change={toggleDirty}
 				/>
 			</label>
 		</div>
