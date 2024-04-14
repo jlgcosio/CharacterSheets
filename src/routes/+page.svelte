e73a1a9c27edd8d99e712af44a3f529c1af65b2f
Jono Luiz Cosio <jonoluiz@gmail.com>
2024-04-14 23:42:22 +0800

    Update home buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,10 @@
 	<div class="container py-12">
 		<div class="grid grid-flow-row justify-center gap-4 align-middle">
 			<div class="card col-auto shadow-md">
-				<a class="card-body" href="/sheets/monster-guts">Monster Guts</a>
+				<a class="card-body" href="/sheets/monster-guts/character">Monster Guts Characters</a>
+			</div>
+			<div class="card col-auto shadow-md">
+				<a class="card-body" href="/sheets/monster-guts/monsters">Monster Guts Monsters</a>
 			</div>
 			<!-- TODO Godbound -->
 			<div class="card col-auto hidden shadow-md">
