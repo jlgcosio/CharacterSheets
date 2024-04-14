a2cfe308ba1b368c480f8e58292ad01f432ceffc
Jono Luiz Cosio <jonoluiz@gmail.com>
2024-04-14 23:43:07 +0800

    Add blank monster option

--- src/routes/sheets/monster-guts/monsters/+page.svelte
+++ src/routes/sheets/monster-guts/monsters/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import Monster from 'components/monsterguts/Monster.svelte';
-	import type { IClock, IMonster } from 'libTypes/MonsterGutsTypes';
+	import type { IMonster } from 'libTypes/MonsterGutsTypes';
 
 	// import type { PageData } from './$types';
 
@@ -8,101 +8,111 @@
 	let monsterList: IMonster[] = [];
 	let files: FileList;
 
-	let monster: IMonster = {
-		id: `monster-${Date.now()}`,
-		name: '',
-		description: '',
-		rage: '',
-		tags: [],
-		moves: [],
-		body: {
-			current: 50,
-			max: 50
-		},
-		parts: [],
-		clocks: {
-			bleed: {
-				current: 0,
-				max: 6,
-				immune: false
-			},
-			fire: {
-				current: 0,
-				max: 6,
-				immune: false
-			},
-			ice: {
-				current: 0,
-				max: 6,
-				immune: false
-			},
-			metal: {
-				current: 0,
-				max: 6,
-				immune: false
-			},
-			shock: {
-				current: 0,
-				max: 6,
-				immune: false
-			},
-			slime: {
-				current: 0,
-				max: 6,
-				immune: false
-			},
-			snooze: {
-				current: 0,
-				max: 6,
-				immune: false
-			},
-			stagger: {
-				current: 0,
-				max: 6,
-				immune: false
-			},
-			stun: {
-				current: 0,
-				max: 6,
-				immune: false
-			},
-			venom: {
-				current: 0,
-				max: 6,
-				immune: false
-			}
-		}
-	};
-
 	async function importFiles() {
 		let list: IMonster[] = [];
 		if (files.length > 0) {
 			for (let index = 0; index < files.length; index++) {
 				const file = await files[index].text();
 				const mon = JSON.parse(file) as unknown as IMonster;
-				list.push(mon);
+				if (monsterList.findIndex((m) => m.id === mon.id) === -1) {
+					list.push(mon);
+				}
 			}
-			monsterList = list;
+			monsterList = [...monsterList, ...list];
 		}
+	}
+
+	function addMonster() {
+		monsterList = [
+			...monsterList,
+			{
+				id: `monster-${Date.now()}`,
+				name: '',
+				description: '',
+				rage: '',
+				tags: [],
+				moves: [],
+				body: {
+					current: 50,
+					max: 50
+				},
+				parts: [],
+				clocks: {
+					bleed: {
+						current: 0,
+						max: 6,
+						immune: false
+					},
+					fire: {
+						current: 0,
+						max: 6,
+						immune: false
+					},
+					ice: {
+						current: 0,
+						max: 6,
+						immune: false
+					},
+					metal: {
+						current: 0,
+						max: 6,
+						immune: false
+					},
+					shock: {
+						current: 0,
+						max: 6,
+						immune: false
+					},
+					slime: {
+						current: 0,
+						max: 6,
+						immune: false
+					},
+					snooze: {
+						current: 0,
+						max: 6,
+						immune: false
+					},
+					stagger: {
+						current: 0,
+						max: 6,
+						immune: false
+					},
+					stun: {
+						current: 0,
+						max: 6,
+						immune: false
+					},
+					venom: {
+						current: 0,
+						max: 6,
+						immune: false
+					}
+				}
+			}
+		];
 	}
 </script>
 
-<section class="my-12">
-	<label class="form-control w-full max-w-xs">
-		<div class="label">
-			<span class="label-text">Pick files to import</span>
+<label class="form-control w-full max-w-xs">
+	<div class="label">
+		<span class="label-text">Import monsters or add new to the list</span>
+	</div>
+	<div class="flex gap-4">
+		<div>
+			<input
+				type="file"
+				class="file-input file-input-bordered"
+				accept="json"
+				multiple={true}
+				bind:files
+				on:change={importFiles}
+			/>
 		</div>
-		<input
-			type="file"
-			class="file-input file-input-bordered w-full max-w-xs"
-			accept="json"
-			multiple={true}
-			bind:files
-			on:change={importFiles}
-		/>
-	</label>
+		<button class="btn btn-primary" on:click={addMonster}>Create new monster</button>
+	</div>
+</label>
 
-	{#each monsterList as monster}
-		<Monster {monster} />
-	{/each}
-</section>
+{#each monsterList as monster}
+	<Monster {monster} />
+{/each}
