40756e29277b165f16f417ed1841890c421b9ad4
Jono Luiz Cosio <jonoluiz@gmail.com>
2024-06-05 20:14:41 +0800

    Add typings

---
+++ src/lib/enums/GodboundEnums.ts
@@ -0,0 +1,13 @@
+export enum Attributes {
+    Strength = 'str',
+    Dexterity = 'dex',
+    Constitution = 'con',
+    Intelligence = 'int',
+    Wisdom = 'wis',
+    Charisma = 'cha'
+}
+
+export enum GiftCategory {
+    Lesser,
+    Greater
+}
